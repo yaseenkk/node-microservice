330f4f8677e0789c1381c5b63b5b9447aca40c46
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2018-12-12 16:30:18 +0600

    Added some contents

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 echo Hello Nazmul
 docker pull nazmulb/node-microservice
-docker run -rm -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
+docker run --rm -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
 docker rmi nazmulb/node-microservice
