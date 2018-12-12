3f3d5edff94e016cb9b1b18f9c119af21c01120d
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2018-12-12 14:00:12 +0600

    Changed some contest

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -2,5 +2,6 @@
 set -e
 
 echo Hello Nazmul
-docker pull nazmulb/node-microservice:latest
-docker run -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
+docker pull nazmulb/node-microservice
+docker run -rm -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
+docker rmi nazmulb/node-microservice
