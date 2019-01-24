5b24e014c1ca64d844fdcb42f485b15d3aee4205
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2019-01-24 11:16:46 +0600

    Added Changes

--- spec.js
+++ spec.js
@@ -20,6 +20,6 @@ describe('loading express', function () {
     it('404 everything else', function testPath(done) {
       request(server)
         .get('/foo/bar')
-        .expect(200, done);
+        .expect(404, done);
     });
 });
