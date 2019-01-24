616c7f643f35d61898298faa56e623bda2959a41
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2019-01-24 12:06:42 +0600

    Added Changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.status(200).send('ok');
+    res.status(200).send('ok2');
 });
 
 var server = app.listen(3000, function () {
