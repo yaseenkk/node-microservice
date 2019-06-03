8c1a2e871348e39fa61e7899d5292b7fc63d8445
nazmulbasher <nazmul.basher@fieldnation.com>
2019-06-03 12:56:00 +0600

    Changed some codes

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 npm start &
 sleep 1
--- jenkins/kill.sh
+++ jenkins/kill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
